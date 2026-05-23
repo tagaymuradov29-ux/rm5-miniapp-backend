c1646d1375364f86852377814d0c9fa40d419f10
tagaymuradov29-ux <tagaymuradov29-ux@github.com>
2026-05-23 21:16:27 +0500

    fix: add pool_pre_ping to fix DB connection closed error

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(DATABASE_URL, echo=False, pool_pre_ping=True, pool_recycle=300, pool_size=5, max_overflow=10)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
